483fb33d2d61f8be7bdd0b1fc59014b0d6232a4f
tatito254 <164010118+tatito254@users.noreply.github.com>
2024-05-28 09:05:53 -0300

    agrego la programacion

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class LoginManager : MonoBehaviour
+{
+    public string contra;
+    public InputField Ingresar_Contra;
+
+    public void ChequearContra()
+    {
+        if (Ingresar_Contra.text == contra)
+        {
+            Debug.Log("Access granted");
+        }
+
+        else
+        {
+            Debug.Log("Access denied");
+
+        }
+    }
+}
